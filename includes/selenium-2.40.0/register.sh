4d457b8d83d20f6c92ff0b2cbe1d8742245b29fb
dylan_foster <dylan_foster@apple.com>
2014-03-13 16:45:53 -0700

    updating grid scripts

--- includes/selenium-2.40.0/register.sh
+++ includes/selenium-2.40.0/register.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -jar selenium-server-standalone-2.40.0.jar -role node -hub http://localhost:4444/grid/regsiter
+java -jar ~/test/AppleLayoutTesting/includes/selenium-2.40.0/selenium-server-standalone-2.40.0.jar -role node -hub http://localhost:4444/grid/regsiter
