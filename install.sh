f6de0f5ad0e66893871c6a201bb2956b25558227
dylan_foster <dylan_foster@apple.com>
2014-03-13 15:47:42 -0700

    modified install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
+echo "Adding Galen and Chromedriver to your PATH
+..
+..
+..
+.."
+
 echo "export PATH=$PATH:~/test/AppleLayoutTesting/includes/selenium-2.40.0/:~/test/AppleLayoutTesting/includes/galen-bin-0.9.0/" >> ~/.profile
 mv ~/test/AppleLayoutTesting/includes/chromedriver /usr/bin/
+. ~/.profile
+
+echo "...Done"
+echo "Your PATH is now"  echo "$PATH"
+echo "
+Reload your profile by typing:  source ~/.profile
+...
+...
+...Then you are ready to start your grid. Just run start.sh then open another termianl and run register.sh"
