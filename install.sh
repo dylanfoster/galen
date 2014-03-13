6e42d4ee2f2d7b41e2f20b065f897b22081f57f5
dylan_foster <dylan_foster@apple.com>
2014-03-13 11:55:52 -0700

    v1.5: Added install.sh

---
+++ install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "export PATH=$PATH:~/test/AppleLayoutTesting/includes/selenium-2.40.0/:~/test/AppleLayoutTesting/includes/galen-bin-0.9.0/" >> ~/.profile
+mv ~/test/AppleLayoutTesting/includes/chromedriver /usr/bin/
