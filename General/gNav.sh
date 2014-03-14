67540ab88717ee9c291ba81ad34e8e8a1d590aa6
dylan_foster <dylan_foster@apple.com>
2014-03-14 15:52:33 -0700

    v1.6.4 fixed scripts to remove files

--- General/gNav.sh
+++ General/gNav.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 galen test GlobalNav.test --htmlreport "reports"
 cd reports
+shopt -s extglob
 now=$(date +'%m%d%y_%H:%M')
-zip report_$now.zip *.*
+zip report_$now.zip *.* -x *.zip
+rm !(*.zip)
